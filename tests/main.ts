262651ed0ec9c754650e43398855e9a74876911f
AndreiLucas123 <andrei.lucas.goncalves@gmail.com>
2025-04-23 23:47:06 -0300

    chore: Bump version to 4.0.0-next.12 and update prepack script

--- tests/main.ts
+++ tests/main.ts
@@ -1,8 +1,6 @@
 import { int } from 'schemas-lib';
-import { storageItem, storageGroup } from '../src/old-version/old-version';
-import { StorageVersioning } from '../src/old-version/StorageVersioning';
 import { storageVersioning } from '../src';
-import { Store } from 'simorg-store';
+import { store } from 'simorg-store';
 
 //
 //
@@ -11,9 +9,5 @@ localStorage.clear();
 
 const schema = int().catch(1);
 
-(window as any).__storageGroup = storageGroup;
-(window as any).__storageItem = storageItem;
 (window as any).__schema = schema;
-(window as any).__StorageVersioning = StorageVersioning;
 (window as any).__currentStorageVersioning = storageVersioning;
-(window as any).__Store = Store;
